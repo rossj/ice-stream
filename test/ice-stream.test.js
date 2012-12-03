756fec8465f20d6d5ef6133a00efbdff3e2f21d2
Ross Johnson <ross@mazira.com>
2012-12-03 12:14:56 -0600

    Add ability to pass string or array to constructor

--- test/ice-stream.test.js
+++ test/ice-stream.test.js
@@ -4,6 +4,7 @@ var
 	Stream = require('stream'),
 
 // Npm modules
+	_ = require('lodash'),
 	should = require('should'),
 
 // Project modules
@@ -13,21 +14,26 @@ var
 	testFilePath = __dirname + '/testfile.txt',
 	testFileData = fs.readFileSync(testFilePath, 'utf-8');
 
-describe('istream', function() {
+describe('istream', function () {
 	/**
 	 * Asserts that the final contents of a stream match the given data
 	 * @param stream
 	 * @param data
 	 */
 	function assertStreamData(stream, data, cb) {
-		var buf = '';
-		stream.on('data', function(chunk) {
-			buf += chunk;
+		var isString = _.isString(data);
+		var buf = isString ? '' : [];
+		stream.on('data', function (chunk) {
+			if ( isString )
+				buf += chunk;
+			else {
+				buf.push(chunk);
+			}
 		});
 
-		stream.on('end', function() {
+		stream.on('end', function () {
 			// Check that the message got through to stdin - we sent it twice
-			buf.should.equal(data);
+			buf.should.eql(data);
 			cb();
 		});
 	}
@@ -40,55 +46,67 @@ describe('istream', function() {
 	 */
 	function assertStreamChunks(stream, data, order, cb) {
 		var pieces = [];
-		stream.on('data', function(chunk) {
+		stream.on('data', function (chunk) {
 			pieces.push(chunk);
 		});
 
-		stream.on('end', function() {
+		stream.on('end', function () {
 			// Check that the message got through to stdin - we sent it twice
-			if (order) {
+			if ( order ) {
 				// Assert the array contents and order match
 				pieces.should.eql(data);
 			} else {
 				// We don't care about order to just make sure the arrays are same size
 				// and contain same elements
 				pieces.length.should.equal(data.length);
-				for (var i = 0; i < data.length; i++) {
-					pieces.should.include(data[i]);
-				}
+				_.difference(pieces, data).should.have.length(0);
 			}
 			cb();
 		});
 	}
 
-	describe('Constructor', function() {
-		it('should return a Streamit object when called without "new"', function() {
+	describe('Constructor', function () {
+		it('should return a Streamit object when called without "new"', function () {
 			var s = istream();
 			s.should.be.an.instanceOf(istream);
 		});
 
-		it('should return a Streamit object when called with "new"', function() {
+		it('should return a Streamit object when called with "new"', function () {
 			var s = new istream();
 			s.should.be.an.instanceOf(istream);
 		});
+
+		it('should accept a string as a parameter and make it a stream', function (cb) {
+			var msg = 'hello this is a string';
+			var s = new istream(msg);
+			s.should.be.an.instanceOf(istream);
+			assertStreamData(s.stream(), msg, cb);
+		});
+
+		it('should accept an array as a parameter and make it a stream', function (cb) {
+			var msg = [1, 5, 2, 5, 2];
+			var s = new istream(msg);
+			s.should.be.an.instanceOf(istream);
+			assertStreamData(s.stream(), msg, cb);
+		});
 	});
-	describe('exec', function() {
-		it('should return a readable, writeable Stream object', function() {
+	describe('exec', function () {
+		it('should return a readable, writeable Stream object', function () {
 			var s = istream.exec('echo hello');
 			s.should.be.an.instanceOf(Stream);
 			s.readable.should.equal(true);
 			s.writable.should.equal(true);
 		});
 
-		it('should emit "error" event if the command is invalid', function(cb) {
+		it('should emit "error" event if the command is invalid', function (cb) {
 			var s = istream.exec('dfkjaf23jfadfadsf');
-			s.on('error', function(err) {
+			s.on('error', function (err) {
 				err.should.be.an.instanceOf(Error);
 				cb();
 			})
 		});
 
-		it('should fire "data" and "end" events with stdout', function(cb) {
+		it('should fire "data" and "end" events with stdout', function (cb) {
 			var msg = 'hello there';
 			var s = istream.exec('echo ' + msg);
 
@@ -96,7 +114,7 @@ describe('istream', function() {
 			assertStreamData(s, msg + '\n', cb);
 		});
 
-		it('should accept #write and #end calls and pass to stdin', function(cb) {
+		it('should accept #write and #end calls and pass to stdin', function (cb) {
 			var msg = 'hello there';
 			var s = istream.exec('cat');
 
@@ -109,47 +127,47 @@ describe('istream', function() {
 		});
 	});
 
-	describe('split', function(cb) {
-		it('should split on newline by default', function(cb) {
+	describe('split', function (cb) {
+		it('should split on newline by default', function (cb) {
 			var msg = 'hello\nhow\nare\nyou\ndoing?';
-			var s = istream().exec('echo ' + msg).split().stream();
+			var s = istream(msg).split().stream();
 			assertStreamChunks(s, msg.split('\n'), true, cb);
 		});
 
-		it('should split on given dilimiter', function(cb) {
+		it('should split on given dilimiter', function (cb) {
 			var msg = 'hello how are you doing?';
-			var s = istream().exec('echo ' + msg).split('o').stream();
-			assertStreamChunks(s, (msg + '\n').split('o'), true, cb);
+			var s = istream(msg).split('o').stream();
+			assertStreamChunks(s, msg.split('o'), true, cb);
 		});
 	});
 
-	describe('filter', function(cb) {
+	describe('filter', function (cb) {
 		var msg = 'get rid of all words that contain the letter e';
 		var result = 'rid of all words that contain';
 
-		it('filter should work with synchronous function and maintain order', function(cb) {
-			var s = istream().exec('echo ' + msg).split(' ').filter(function(chunk) {
+		it('filter should work with synchronous function and maintain order', function (cb) {
+			var s = istream(msg).split(' ').filter(function (chunk) {
 				return chunk.indexOf('e') === -1;
 			}).stream();
 			assertStreamChunks(s, result.split(' '), true, cb);
 		});
 
-		it('filterAsync should work with async function and maybe maintain order', function(cb) {
-			var s = istream().exec('echo ' + msg).split(' ').filterAsync(function(chunk, cb) {
+		it('filterAsync should work with async function and maybe maintain order', function (cb) {
+			var s = istream(msg).split(' ').filterAsync(function (chunk, cb) {
 				// Simulate some varying callback times
-				var ms = Math.floor((Math.random()*500)+1);
-				setTimeout(function() {
+				var ms = Math.floor((Math.random() * 500) + 1);
+				setTimeout(function () {
 					cb(chunk.indexOf('e') === -1);
 				}, ms);
 			}).stream();
 			assertStreamChunks(s, result.split(' '), false, cb);
 		});
 
-		it('filterAsyncSeries should work with async function and maintain order', function(cb) {
-			var s = istream().exec('echo ' + msg).split(' ').filterAsyncSeries(function(chunk, cb) {
+		it('filterAsyncSeries should work with async function and maintain order', function (cb) {
+			var s = istream(msg).split(' ').filterAsyncSeries(function (chunk, cb) {
 				// Simulate some varying callback times
-				var ms = Math.floor((Math.random()*500)+1);
-				setTimeout(function() {
+				var ms = Math.floor((Math.random() * 500) + 1);
+				setTimeout(function () {
 					cb(chunk.indexOf('e') === -1);
 				}, ms);
 			}).stream();
@@ -157,15 +175,14 @@ describe('istream', function() {
 		});
 	});
 
-	describe('chaining', function(cb) {
-		it('should pipe output down a whole chain starting with a ice-stream object', function(cb) {
+	describe('chaining', function (cb) {
+		it('should pipe output down a whole chain starting with a ice-stream object', function (cb) {
 			var msg = 'hello there how are you';
-			var stream = istream.exec('echo ' + msg);
-			var s = istream(stream).exec('cat').exec('cat').exec('cat').exec('cat').stream();
-			assertStreamData(s, msg + '\n', cb);
+			var s = istream(msg).exec('cat').exec('cat').exec('cat').exec('cat').stream();
+			assertStreamData(s, msg, cb);
 		});
 
-		it('should pipe output down a whole chain starting with nothing', function(cb) {
+		it('should pipe output down a whole chain starting with nothing', function (cb) {
 			var msg = 'hello there how are you';
 			var s = istream.chain().exec('echo ' + msg).exec('cat').exec('cat').exec('cat').stream();
 			assertStreamData(s, msg + '\n', cb);
