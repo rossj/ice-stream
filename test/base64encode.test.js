2b1078554fee3b38337ffb13d2b14c7b9ab1f61f
Ross Johnson <ross@mazira.com>
2013-07-08 02:55:50 -0500

    Add base64encode with tests

---
+++ test/base64encode.test.js
@@ -0,0 +1,65 @@
+/*global describe, it*/
+var
+// Node modules
+	fs = require('fs'),
+
+// Npm modules
+	_ = require('lodash'),
+	should = require('should'),
+
+// Project modules
+	base64decode = require('../lib/base64encode.js');
+
+describe('base64encode', function () {
+	/**
+	 * This function emits an array of string chunks to the stream, and then compares the output to a given value.
+	 * @param stream
+	 * @param inputs
+	 * @param output
+	 */
+	function testStream(stream, inputs, output) {
+		for (var i = 0; i < inputs.length; i++)
+			stream.write(inputs[i]);
+
+		stream.end();
+
+		var result = stream.read();
+		result.should.be.an.instanceOf(Buffer);
+		result.toString().should.equal(output);
+	}
+
+	describe('input in a single chunk', function () {
+		it('should properly encode a string', function () {
+			testStream(base64decode(), ['any carnal pleasur'], 'YW55IGNhcm5hbCBwbGVhc3Vy');
+		});
+
+		it('should properly encode a Buffer', function () {
+			testStream(base64decode(), [new Buffer('any carnal pleasur')], 'YW55IGNhcm5hbCBwbGVhc3Vy');
+		});
+
+		it('should properly encode a Buffer and include padding', function () {
+			testStream(base64decode(), ['any carnal pleasure.'], 'YW55IGNhcm5hbCBwbGVhc3VyZS4=');
+		});
+	});
+
+	describe('input in multiple chunks, lengths divisible by 3', function () {
+		it('should properly encode a Buffer', function () {
+			testStream(base64decode(), [new Buffer('any ca'), new Buffer('rnal p'), new Buffer('leasur')], 'YW55IGNhcm5hbCBwbGVhc3Vy');
+		});
+
+		it('should properly encode a Buffer and include padding', function () {
+			testStream(base64decode(), [new Buffer('any ca'), new Buffer('rnal p'), new Buffer('leasure.')], 'YW55IGNhcm5hbCBwbGVhc3VyZS4=');
+		});
+	});
+
+	describe('input in multiple chunks, lengths not divisible by 3', function () {
+		it('should properly encode a Buffer', function () {
+			testStream(base64decode(), [new Buffer('any carn'), new Buffer('al pl'), new Buffer('easur')], 'YW55IGNhcm5hbCBwbGVhc3Vy');
+		});
+
+		it('should properly encode a Buffer and include padding', function () {
+			testStream(base64decode(), [new Buffer('any carn'), new Buffer('al pl'), new Buffer('easure.')], 'YW55IGNhcm5hbCBwbGVhc3VyZS4=');
+		});
+	});
+});
+
